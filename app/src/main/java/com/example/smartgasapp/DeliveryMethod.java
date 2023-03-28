11007cd26ed3d1d89b66aeb7e48e2d8186e2a2b7
linda <linda62345@gmail.com>
2023-03-28 20:42:13 +0800

    delivery_method

---
+++ app/src/main/java/com/example/smartgasapp/DeliveryMethod.java
@@ -0,0 +1,60 @@
+package com.example.smartgasapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
+
+public class DeliveryMethod extends AppCompatActivity {
+    private Button order;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_method);
+        order = findViewById(R.id.ConfrimOrder);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DeliveryMethod.this, OrderDetail.class);
+                startActivity(intent);
+            }
+        });
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.nav_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.navigation_dashboard:
+                        startActivity(new Intent(getApplicationContext(),UserDashboard.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.navigation_home:
+                        startActivity(new Intent(getApplicationContext(),Homepage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.navigation_notifications:
+                        startActivity(new Intent(getApplicationContext(),OrderListUnfinished.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
